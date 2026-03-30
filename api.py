9fd718aea7550e64c71b4ce55b11bd6a2b7b793e
stamp10537-byte <stamp10537@gmail.com>
2026-03-30 13:02:02 +0700

    Update api.py

--- api.py
+++ api.py
@@ -48,7 +48,7 @@ def save_db(data):
 # =========================================================
 # 💡 เปลี่ยนลิงก์เชื่อมต่อเป็น Supabase Session Pooler ของบอสแล้วครับ!
 # ⚠️ สำคัญมาก: ก่อนรันไฟล์นี้ บอสต้องลบคำว่า [YOUR-PASSWORD] รวมทั้งวงเล็บ [] ออก แล้วพิมพ์รหัสผ่านที่บอสตั้งไว้แทนนะครับ
-DB_URI = postgresql://postgres.ekwhfctojnjeglcyxxvh:StampOracle2026@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres
+DB_URI = "postgresql://postgres.ekwhfctojnjeglcyxxvh:StampOracle2026@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres"
 
 def get_db_connection(): return psycopg2.connect(DB_URI)
 
